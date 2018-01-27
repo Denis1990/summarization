e3baf71c0778304d051508eec78006b5f76fe8fa
denis mone <monedenis@gmail.com>
2018-01-27 19:55:01 +0200

    Bug fix. Construct the string with boundaries parameters, in order to prevent the program filling it with \\u0000.

--- src/main/java/nnkstemmer/NNKStemmerAdapter.java
+++ src/main/java/nnkstemmer/NNKStemmerAdapter.java
@@ -2,6 +2,7 @@ package nnkstemmer;
 
 import java.util.ArrayList;
 
+import static nnkstemmer.nnkstem.greek_vowel;
 import static nnkstemmer.nnkstem.rswas;
 
 public class NNKStemmerAdapter {
@@ -9,7 +10,10 @@ public class NNKStemmerAdapter {
 
     public static int stemWord(char []data, int size) {
         try {
-            words.add(new word_node(String.valueOf(data)));
+            // use the portion of the array that has characters
+            // otherwise the entire data array is filled, and the
+            // space that is leftover is filled with \\u0000
+            words.add(new word_node(String.valueOf(data, 0, size)));
             rswas(words);
             final word_node wn = words.remove(0);
             if (wn.getType() == word_node.ShortWord) {
@@ -17,7 +21,7 @@ public class NNKStemmerAdapter {
             } else if (wn.getType() == word_node.StopWord) {
                 return size;
             }
-            return wn.getNormalized().length();
+            return (wn.getNormalized() == null) ? wn.getWord().length() : wn.getNormalized().length();
         } catch (NullPointerException npe){
             return 4;
         }

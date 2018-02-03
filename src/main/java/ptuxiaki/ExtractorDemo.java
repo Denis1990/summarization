e4394c787dd78f180cffe28d37cbd3377a1ddabb
denis mone <monedenis@gmail.com>
2018-02-03 15:18:45 +0200

    Print sentences for each file

--- src/main/java/ptuxiaki/ExtractorDemo.java
+++ src/main/java/ptuxiaki/ExtractorDemo.java
@@ -5,17 +5,21 @@ import org.xml.sax.SAXException;
 import ptuxiaki.extraction.TextExtractor;
 
 import java.io.IOException;
+import java.text.BreakIterator;
 import java.util.List;
+import java.util.Locale;
 
 public class ExtractorDemo {
 
     public static void main(String[] args) throws IOException, TikaException, SAXException {
-        //FileOutputStream fos = new FileOutputStream(new File("sentences_found.txt"));
         TextExtractor extractor = new TextExtractor();
-        List<String> s = extractor.extractSentencesFromFile(args[0]);
-        int j = 0;
-        for (String i : s) {
-            System.out.println(String.format("%d sentence: %s", ++j, i.replaceAll("\\s", " ")));
+        for (String arg : args) {
+            System.out.println("=====" + arg + "======");
+            List<String> s = extractor.extractSentencesFromFile(arg);
+            int j = 0;
+            for (String i : s) {
+                System.out.println(String.format("%d sentence: %s", ++j, i.replaceAll("\\s", " ")));
+            }
         }
     }
 }

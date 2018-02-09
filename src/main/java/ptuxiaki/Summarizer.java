8163e7497826d13d575941e5217ee56c0d9eaa98
Denis Mone <monedenis@gmail.com>
2018-02-09 18:22:28 +0200

    Implement log functions in a separate class

--- src/main/java/ptuxiaki/Summarizer.java
+++ src/main/java/ptuxiaki/Summarizer.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import ptuxiaki.datastructures.SentenceType;
 import ptuxiaki.extraction.TextExtractor;
 import ptuxiaki.indexing.Indexer;
+import ptuxiaki.utils.MathUtils;
 import ptuxiaki.utils.SentenceUtils;
 
 import java.io.File;
@@ -21,8 +22,10 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.lang.Math.log10;
+import static java.lang.Math.round;
+import static ptuxiaki.utils.MathUtils.log2;
+import static ptuxiaki.utils.MathUtils.log3;
 import static ptuxiaki.utils.PropertyKey.*;
-import static ptuxiaki.utils.SentenceUtils.keywords;
 import static ptuxiaki.utils.SentenceUtils.stemSentence;
 
 
@@ -60,6 +63,7 @@ public class Summarizer {
         //TODO: implement logarithmic calculation
         double a = Double.valueOf(properties.getProperty("a", "0.6"));
         double b = Double.valueOf(properties.getProperty("b", "0.4"));
+        int sentWords = sentence.split("\\s+").length;
         int tt = 0, mtt = 0;
         for (Pair<String, SentenceType> word : titleWords) {
             if (stemSentence(sentence).contains(word.getKey())) {
@@ -70,7 +74,7 @@ public class Summarizer {
                 }
             }
         }
-        return (a * tt) + (b * mtt);
+        return (a * (log2(tt)/log2(sentWords))) + (b * (log3(mtt) / log3(sentWords)));
     }
 
     private void summarizeFile(final String filePath, int docId) throws IOException {
@@ -136,7 +140,7 @@ public class Summarizer {
             // use log functions to determine importance
             // see paper B47
             // TODO: implement keyword for each title and subtitle.
-            tt[i] = titleKeywords(stemSentence(sentences.get(i)), titleWords);
+            tt[i] = titleKeywords(sentences.get(i), titleWords);
             // this is a problem. 0 indicates the number of the document, in the order it was indexed
             // what is that order i don't know. Maybe alphabetical, maybe by type or size.
             // The point is i need a way to link the indexed document with an integer because the tf() method needs one
@@ -171,7 +175,7 @@ public class Summarizer {
 
         // this should have been loaded above
         double compress = (double) Integer.parseInt(properties.getProperty(COMPRESS)) / 100;
-        int summarySents = Ints.saturatedCast(size - (Math.round(size * compress)));
+        int summarySents = Ints.saturatedCast(size - (round(size * compress)));
 
         weights.sort(Comparator.reverseOrder());
         int begin = filePath.lastIndexOf(File.separatorChar);
---
+++ src/main/java/ptuxiaki/utils/MathUtils.java
@@ -0,0 +1,30 @@
+package ptuxiaki.utils;
+
+import static java.lang.Math.log;
+import static java.lang.Math.log1p;
+
+public class MathUtils {
+
+    /**
+     * Return the log with base 3 of value (x + 1).
+     * Use math identity:
+     * logb(n) = loge(n) / loge(b)
+     * @param x
+     * @return
+     */
+    public static double log3(int x) {
+        return log1p((double)x) / log1p((double)3);
+    }
+
+    /**
+     * Return the log with base 2 of value (x + 2).
+     * Use math identity:
+     * logb(n) = loge(n) / loge(b)
+     * @param x
+     * @return
+     */
+    public static double log2(int x) {
+        return log((double)x + 2) / log((double)2);
+    }
+
+}

db8ca613aff49e5b7ea1e5cb30844757e3e7b861
Denis Mone <monedenis@gmail.com>
2018-03-07 20:07:50 +0200

    Bash script to build all artifacts

---
+++ build_artifacts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ ! -d "dist/" ]]; then
+    mkdir 'dist'
+fi
+
+mvn clean
+
+mvn -Pdemo -DskipTests=true package
+cp target/summarization.jar dist/demoLucene.jar
+
+mvn -Pextractor -DskipTests=true package
+cp target/summarization.jar dist/extractorDemo.jar
+
+mvn -Pdev package
+cp target/summarization.jar dist/
+
+cp -r target/dependency dist/
+
+if [[ ! -d "$HOME/Documents/demoLucene" ]]; then
+    echo "[WARNING] $HOME/Documents/demoLucene directory does not exist!!!"
+    echo "[WARNING] Create directory and populate it with documents in order to run the demo."
+fi
+

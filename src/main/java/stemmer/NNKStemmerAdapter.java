d89fa002b0a3b2dc02cb07c6d9ed41f1142b9093
denis mone <monedenis@gmail.com>
2018-03-14 21:50:31 +0200

    Suround in try/catch

--- src/main/java/stemmer/NNKStemmerAdapter.java
+++ src/main/java/stemmer/NNKStemmerAdapter.java
@@ -2,6 +2,7 @@ package stemmer;
 
 
 import nnkstemmer.word_node;
+import org.apache.commons.collections4.multiset.SynchronizedMultiSet;
 
 import java.util.ArrayList;
 
@@ -11,17 +12,23 @@ public class NNKStemmerAdapter {
     static ArrayList<nnkstemmer.word_node> words = new ArrayList<>();
 
     public static int stemWord(char []data, int size) {
-        // use the portion of the array that has characters
-        // otherwise the entire data array is filled, and the
-        // space that is leftover is filled with \\u0000
-        words.add(new word_node(String.valueOf(data, 0, size)));
-        rswas(words);
-        final word_node wn = words.remove(0);
-        if (wn.getType() == word_node.ShortWord) {
-            return size;
-        } else if (wn.getType() == word_node.StopWord) {
+        try {
+            // use the portion of the array that has characters
+            // otherwise the entire data array is filled, and the
+            // space that is leftover is filled with \\u0000
+            words.add(new word_node(String.valueOf(data, 0, size)));
+            rswas(words);
+            final word_node wn = words.remove(0);
+            if (wn.getType() == word_node.ShortWord) {
+                return size;
+            } else if (wn.getType() == word_node.StopWord) {
+                return size;
+            }
+            return (wn.getNormalized() == null) ? wn.getWord().length() : wn.getNormalized().length();
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+            System.out.println(words.remove(0));
             return size;
         }
-        return (wn.getNormalized() == null) ? wn.getWord().length() : wn.getNormalized().length();
     }
 }

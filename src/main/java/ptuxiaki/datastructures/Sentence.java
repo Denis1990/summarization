8a5c1d6df63a6afa0b7410085402b4104e037b95
denis mone <monedenis@gmail.com>
2018-06-03 12:23:40 +0300

    Add additional constructor on Sentence class

--- src/main/java/ptuxiaki/datastructures/Sentence.java
+++ src/main/java/ptuxiaki/datastructures/Sentence.java
@@ -30,12 +30,26 @@ public class Sentence {
     public final int position;
 
     /**
+     * Position inside the paragraph
+     */
+    public final int parPosition;
+
+    /**
      * Construct a sentence with the given text.
      * This is considered the first sentence by default
      * @param text
      */
     public Sentence(String text) {
-        this(text, SentenceType.SENTENCE, 0);
+        this(text, SentenceType.SENTENCE, 0, 1);
+    }
+
+    /**
+     *
+     * @param text
+     * @param position
+     */
+    public Sentence(String text, int position) {
+        this(text, SentenceType.SENTENCE, position, parPosition);
     }
 
     /**
@@ -44,9 +58,10 @@ public class Sentence {
      * This is considered a normal sentence.
      * @param text
      * @param position
+     * @param parPosition
      */
-    public Sentence(String text, int position) {
-        this(text, SentenceType.SENTENCE, position);
+    public Sentence(String text, int position, int parPosition) {
+        this(text, SentenceType.SENTENCE, position, parPosition);
     }
 
     /**
@@ -57,10 +72,11 @@ public class Sentence {
      * @param type
      * @param position
      */
-    public Sentence(String text, SentenceType type, int position) {
+    public Sentence(String text, SentenceType type, int position, int parPosition) {
         this.text = text;
         this.type = type;
         this.position = position;
+        this.parPosition = parPosition;
         this.weight = 0.0;
         this.titleTerm = 0.0;
     }
